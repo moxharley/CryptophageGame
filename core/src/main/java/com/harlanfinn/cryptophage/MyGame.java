29d93258b8a1b29d2ad462c0072047d75c5337f9
Harlan Bullock <harlanbull7@gmail.com>
2026-02-18 16:27:52 -0800

    Sets up LibGDX

---
+++ core/src/main/java/com/harlanfinn/cryptophage/MyGame.java
@@ -0,0 +1,11 @@
+package com.harlanfinn.cryptophage;
+
+import com.badlogic.gdx.Game;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class MyGame extends Game {
+    @Override
+    public void create() {
+        setScreen(new FirstScreen());
+    }
+}
