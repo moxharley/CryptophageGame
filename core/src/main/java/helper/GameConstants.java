03221f6ea67494f9fb12685fb776b504e038e7a9
FinnWylie <findlaywylie@gmail.com>
2026-03-16 14:20:26 -0700

    Refactor to be global constants

---
+++ core/src/main/java/helper/GameConstants.java
@@ -0,0 +1,21 @@
+package helper;
+
+public class GameConstants {
+
+    private GameConstants() { }
+
+    /**
+     * Represents the Pixels Per Meter scale as a float.
+     */
+    public static final float PPM = 32;
+
+    /**
+     * Represents the Frames Per Second used as a float
+     */
+    public static final int FPS = 60;
+
+    /**
+     * Represents the global Gravity in meters per second as a float
+     */
+    public static final float GRAVITY = 25f; // Unit is m/s. May need to be the negative for certain functions
+}
