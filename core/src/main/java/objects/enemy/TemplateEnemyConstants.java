5962054605eaac01850f03e84f0e5035c7e034a3
FinnWylie <findlaywylie@gmail.com>
2026-03-16 14:21:37 -0700

    Create templates for specific class constants

---
+++ core/src/main/java/objects/enemy/TemplateEnemyConstants.java
@@ -0,0 +1,24 @@
+package objects.enemy;
+
+public class TemplateEnemyConstants {
+
+    private TemplateEnemyConstants() { }
+
+    protected float DEFAULT_WIDTH;
+    protected float DEFAULT_HEIGHT;
+
+    protected double DEFAULT_MAX_HEALTH_POINTS;
+    protected double DEFAULT_CURRENT_HEALTH_POINTS;
+
+    protected double DEFAULT_MOVE_SPEED;
+    protected double DEFAULT_JUMP_HEIGHT;
+
+    protected double DEFAULT_ATTACK_SPEED;
+    protected double DEFAULT_DAMAGE;
+
+    protected double DEFAULT_CRIT_CHANCE;
+    protected double DEFAULT_CRIT_DAMAGE;
+
+    protected double DEFAULT_RESISTANCE;
+    protected double DEFAULT_BULLET_SPEED;
+}
---
+++ core/src/main/java/objects/player/TemplatePlayerEntityConstants.java
@@ -0,0 +1,100 @@
+package objects.player;
+
+/**
+ * Represents the constants used by the PlayerEntity Class.
+ *
+ * @author FinnWylie
+ * @version 2026
+ */
+
+public class TemplatePlayerEntityConstants {
+
+    // TODO: floats are bonus percents.
+    // add all defaults as these aren't instance vars and need something to be reset to
+    //      these are what a new obj is constructed with.
+
+
+
+    private TemplatePlayerEntityConstants() { }
+
+
+
+    /**
+     * Default max health of the player.
+     */
+    public static final int DEFAULT_MAX_HEALTH_POINTS = 24;
+
+    /**
+     * Base amount for damage taken by to be reduced by.
+     */
+    public static final float BASE_RESISTANCE = 0f;
+
+    //----------------------------------------------------------------------------------------------
+
+    /**
+     * Base movement speed of the player.
+     */
+    public static final float BASE_MOVE_SPEED = 10f;
+
+    /**
+     * Base movement speed modifier of the player.
+     */
+    public static final float BASE_MOVE_SPEED_MODIFIER = 0f;
+
+    /**
+     * Base jump velocity of the player.
+     */
+    public static final float BASE_JUMP_VELOCITY = 18f;
+
+    /**
+     * Base jump velocity modifier of the player.
+     */
+    public static final float BASE_JUMP_VELOCITY_MODIFIER = 0f;
+
+    //----------------------------------------------------------------------------------------------
+
+    /**
+     * Base attack speed of the player.
+     */
+    public static final float BASE_ATTACK_SPEED = 5f;
+
+    /**
+     * Base attack speed modifier of the player.
+     */
+    public static final float BASE_ATTACK_SPEED_MODIFIER = 0f;
+
+    /**
+     * Base damage done by the player.
+     */
+    public static final int BASE_DAMAGE = 8;
+
+    /**
+     * Modifier of base damage done by the player.
+     */
+    public static final int BASE_DAMAGE_MODIFIER = 8;
+
+    /**
+     * Base speed of the bullets shot by the player.
+     */
+    public static final float BASE_BULLET_SPEED = 10f;
+
+    /**
+     * Base modifier of the speed of the bullets shot by the player.
+     */
+    public static final float BASE_BULLET_SPEED_MODIFIER = 0f;
+
+    //----------------------------------------------------------------------------------------------
+
+    /**
+     * The percent chance of dealing a critical hit, dealing bonus damage
+     */
+    public static final float BASE_CRIT_CHANCE = 0f;
+
+    /**
+     * On crit, bonus damage dealt is increased by this percent.
+     */
+    public static final float BASE_CRIT_DAMAGE = 0f;
+
+
+
+}
