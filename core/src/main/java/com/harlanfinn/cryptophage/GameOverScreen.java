253a1df3087e95d886f8c9f255d0838347a0674d
Harlan Bullock <harlanbull7@gmail.com>
2026-04-14 12:43:52 -0700

    Adds game over screen

---
+++ core/src/main/java/com/harlanfinn/cryptophage/GameOverScreen.java
@@ -0,0 +1,80 @@
+package com.harlanfinn.cryptophage;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class GameOverScreen extends ScreenAdapter {
+
+    private final MyGameRoot game;
+    private Stage stage;
+    private Skin skin;
+
+    public GameOverScreen(final MyGameRoot game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+
+        skin = new Skin(Gdx.files.internal("uiskin.json"));
+
+        final Label title = new Label("You Died", skin);
+        final TextButton backButton = new TextButton("Back", skin);
+
+        backButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(final ChangeEvent event, final Actor actor) {
+                game.setScreen(new MainMenuScreen(game));
+            }
+        });
+
+        final Table table = new Table();
+        table.setFillParent(true);
+        table.top().left();
+        table.add(backButton).width(90).height(25).pad(10);
+
+        final Table table2 = new Table();
+        table2.setFillParent(true);
+        table2.add(title).padBottom(30);
+
+        stage.addActor(table);
+        stage.addActor(table2);
+    }
+
+    @Override
+    public void render(final float delta) {
+        Gdx.gl.glClearColor(0.05f, 0.05f, 0.08f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override
+    public void resize(final int width, final int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void hide() {
+        Gdx.input.setInputProcessor(null);
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+        skin.dispose();
+    }
+}
+
