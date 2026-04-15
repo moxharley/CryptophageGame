5a01fbcf1a7f68dccbce501f32d14256248e966e
Harlan Bullock <harlanbull7@gmail.com>
2026-04-15 13:06:52 -0700

    Adds score submission to GameOverScreen

--- core/src/main/java/com/harlanfinn/cryptophage/GameOverScreen.java
+++ core/src/main/java/com/harlanfinn/cryptophage/GameOverScreen.java
@@ -4,13 +4,12 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import save.HighScoreManager;
+import save.ScoreEntry;
 
 public class GameOverScreen extends ScreenAdapter {
 
@@ -32,18 +31,31 @@ public class GameOverScreen extends ScreenAdapter {
         skin = new Skin(Gdx.files.internal("uiskin.json"));
 
         final Label title = new Label("You Died", skin);
-        final TextButton backButton = new TextButton("Back", skin);
+        final Label scoreDisplay = new Label("Score: " + score, skin);
+        final TextButton backButton = new TextButton("Submit Score", skin);
+        final TextField nameEntry = new TextField("", skin);
+        nameEntry.setMessageText("Click here to type your name...");
 
         backButton.addListener(new ChangeListener() {
             @Override
             public void changed(final ChangeEvent event, final Actor actor) {
+                String input = nameEntry.getText();
+                if (input.isEmpty()) {
+                    input = "Player";
+                }
+                ScoreEntry newScore = new ScoreEntry(score, input);
+                HighScoreManager.addScore(newScore);
                 game.setScreen(new MainMenuScreen(game));
             }
         });
 
         final Table table = new Table();
         table.setFillParent(true);
-        table.add(title).padBottom(30);
+        table.add(title);
+        table.row();
+        table.add(scoreDisplay).padBottom(10);
+        table.row();
+        table.add(nameEntry).width(200).pad(30);
         table.row();
         table.add(backButton).width(200).pad(10);
 
