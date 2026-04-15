f100554c6f06d26dd309bc83be2f166c7800372c
FinnWylie <findlaywylie@gmail.com>
2026-04-15 13:39:00 -0700

    Add JavaDocs

--- core/src/main/java/objects/entitiy/enemy/EnemyManager.java
+++ core/src/main/java/objects/entitiy/enemy/EnemyManager.java
@@ -4,17 +4,25 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.World;
-import objects.projectile.Projectile;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a EnemyManager.
+ * @author FinnWylie
+ * @version 2026
+ */
 public class EnemyManager {
-    //  todo: change to enemy
     private ArrayList<Enemy> enemyList;
-    private World world;
+    private final World world;
     private SpriteBatch batch;
     private int enemyDeathTotal;
 
+    /**
+     * Creates a new enemy manager.
+     * @param world the Box2D world the enemies will exist in as a World
+     * @param batch the sprite batch the enemies will be rendered by as a Batch
+     */
     public EnemyManager(final World world, final SpriteBatch batch) {
         this.world = world;
         this.batch = batch;
@@ -24,16 +32,14 @@ public class EnemyManager {
         enemyList = new ArrayList<>();
     }
 
-    public void addEnemy(final float width, final float height, final Body body, final EnemyType enemyType) {
-
-        switch (enemyType) {
-            case FLYING_1:
-                enemyList.add(new Enemy1(width, height, body));
-                break;
-            default:
-                enemyList.add(new Enemy1(width, height, body));
-                break;
-        }
+    /**
+     * Adds a new enemy to this EnemyManager.
+     * @param width the width of the enemy to add as a float
+     * @param height the width of the enemy to add as a float
+     * @param body the body of the enemy to add as a Body
+     */
+    public void addEnemy(final float width, final float height, final Body body) {
+        enemyList.add(new Enemy1(width, height, body));
 
     }
 
@@ -41,6 +47,10 @@ public class EnemyManager {
         return world;
     }
 
+    /**
+     * Updates all enemies in this EnemyManager.
+     * @param playerPosition the player's position to be used as a target as a Vector2
+     */
     public void update(final Vector2 playerPosition) {
         ArrayList<Enemy> enemiesToRemove = new ArrayList<>();
         for (Enemy enemy : enemyList) {
@@ -49,26 +59,51 @@ public class EnemyManager {
             if (enemy.getIsDead()) {
                 enemiesToRemove.add(enemy);
                 setEnemyDeathTotal(getEnemyDeathTotal() + 1);
+                getWorld().destroyBody(enemy.getBody());
             }
         }
         enemyList.removeAll(enemiesToRemove);
     }
 
+    /**
+     * Renders each enemy.
+     */
     public void render() {
         for (Enemy enemy : enemyList) {
             enemy.render(batch);
         }
     }
 
+    /**
+     * Returns an arrayList of all enemies.
+     * @return an arrayList containing all enemies
+     * TODO: FIX THIS BAD PRACTICE
+     */
     public ArrayList<Enemy> getEnemyList() {
         return enemyList;
     }
 
+    /**
+     * Returns the enemy death total.
+     * @return enemyDeathTotal as an int.
+     */
     public int getEnemyDeathTotal() {
         return enemyDeathTotal;
     }
 
+    /**
+     * Sets enemyDeathTotal.
+     * @param enemyDeathTotal the new enemyDeathTotal
+     */
     public void setEnemyDeathTotal(final int enemyDeathTotal) {
         this.enemyDeathTotal = enemyDeathTotal;
+    }
+
+    /**
+     * Returns if this enemyManager has no enemies in it.
+     * @return if any enemies are in this enemyManager as a boolean
+     */
+    public boolean isEmpty() {
+        return getEnemyList().isEmpty();
     }
 }
