c477ea3674b02592a8afad62245e74b820a96003
FinnWylie <findlaywylie@gmail.com>
2026-04-16 19:49:49 -0700

    Add Javadocs

--- core/src/main/java/objects/entitiy/player/Player.java
+++ core/src/main/java/objects/entitiy/player/Player.java
@@ -1,6 +1,5 @@
 package objects.entitiy.player;
 
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -9,43 +8,49 @@ import objects.projectile.ProjectileColour;
 import objects.projectile.ProjectileShape;
 import objects.projectile.ProjectileTeam;
 
-import java.util.Random;
-
-import static helper.GameConstants.*;
-import static objects.entitiy.player.TechknightPlayerEntityConstants.*;
 import static objects.entitiy.player.TechknightPlayerEntityConstants.BASE_ATTACK_SPEED;
 import static objects.entitiy.player.TechknightPlayerEntityConstants.BASE_ATTACK_SPEED_MODIFIER;
 import static objects.entitiy.player.TechknightPlayerEntityConstants.BASE_BULLET_SPEED;
 import static objects.entitiy.player.TechknightPlayerEntityConstants.BASE_BULLET_SPEED_MODIFIER;
-import static objects.entitiy.player.TechknightPlayerEntityConstants.BASE_CRIT_CHANCE;
-import static objects.entitiy.player.TechknightPlayerEntityConstants.BASE_CRIT_DAMAGE;
 import static objects.entitiy.player.TechknightPlayerEntityConstants.BASE_DAMAGE;
 import static objects.entitiy.player.TechknightPlayerEntityConstants.BASE_DAMAGE_MODIFIER;
-import static objects.entitiy.player.TechknightPlayerEntityConstants.BASE_JUMP_VELOCITY_MODIFIER;
-import static objects.entitiy.player.TechknightPlayerEntityConstants.BASE_MOVE_SPEED_MODIFIER;
-import static objects.entitiy.player.TechknightPlayerEntityConstants.BASE_RESISTANCE;
 import static objects.entitiy.player.TechknightPlayerEntityConstants.DEFAULT_MAX_HEALTH_POINTS;
+import static objects.entitiy.player.TechknightPlayerEntityConstants.PLAYER_TEXTURE;
+import static objects.entitiy.player.TechknightPlayerEntityConstants.BASE_MOVE_SPEED;
+import static objects.entitiy.player.TechknightPlayerEntityConstants.BASE_BULLET_LIFESPAN;
+import static objects.entitiy.player.TechknightPlayerEntityConstants.DEFAULT_PROJECTILE_SIZE;
+import static objects.entitiy.player.TechknightPlayerEntityConstants.DEFAULT_PROJECTILE_SHAPE;
+import static objects.entitiy.player.TechknightPlayerEntityConstants.DEFAULT_PROJECTILE_COLOUR;
+import static objects.entitiy.player.TechknightPlayerEntityConstants.DEFAULT_PROJECTILE_TEAM;
 
+/**
+ * Represents a Player in the game.
+ * @author FinnWylie
+ * @version 2026
+ */
 public class Player extends GameEntity {
 
+    private final int attackSpeed;
+    private final float attackSpeedModifier;
+    private final float damage;
+    private final float damageModifier;
+    private final float bulletSpeed;
+    private final float bulletSpeedModifier;
+    private final int bulletLifespan;
+    private final int projectileSize;
+    private final ProjectileShape projectileShape;
+    private final ProjectileColour projectileColour;
+    private final ProjectileTeam projectileTeam;
+
     private int currentHealthPoints;
-
-    // TODO: ATTACK speed modifier is better low as opposed to others, fix logic
-    private int attackSpeed;
-    private float attackSpeedModifier;
-    private float damage;
-    private float damageModifier;
-    private float bulletSpeed;
-    private float bulletSpeedModifier;
-    private int bulletLifespan;
-    private int projectileSize;
-
-    private ProjectileShape projectileShape;
-    private ProjectileColour projectileColour;
-    private ProjectileTeam projectileTeam;
-
     private int timeSinceLastShot;
 
+    /**
+     * Constructs a new player with the default values and a custom width, height and body.
+     * @param width width of the player as a float
+     * @param height height of the player as a float
+     * @param body the Box2D body of the player as a Body
+     */
     public Player(final float width, final float height, final Body body) {
         super(width, height, body, PLAYER_TEXTURE);
 
@@ -70,6 +75,10 @@ public class Player extends GameEntity {
         timeSinceLastShot = 0;
     }
 
+    /**
+     * Updates this player object. It is designed to be called every frame.
+     */
+    @Override
     public void update() {
         // makes the body move based on velocity and speed
         getBody().setLinearVelocity(getVelX() * getSpeed(), getVelY() * getSpeed());
@@ -82,24 +91,37 @@ public class Player extends GameEntity {
         moveHitbox();
 
         setTimeSinceLastShot(getTimeSinceLastShot() + 1);
-//        setTimeSinceLastSkill(getTimeSinceLastSkill() + 1);
 
         if (getCurrentHealthPoints() <= 0) {
             setDead(true);
         }
     }
 
+    /**
+     * Renders the sprite associated with this Player at the location of it's X and Y coordinates.
+     * @param batch the batch to render this GameEntity in as a SpriteBatch
+     */
     @Override
     public void render(final SpriteBatch batch) {
         batch.draw(getSprite(), getX() - (getWidth() / 2),
             getY() - (getHeight() / 2), getWidth(), getHeight());
     }
 
+    /**
+     * Moves this player in a direction.
+     * @param horizontalMovement the value to set this player's horizontal velocity by as an int
+     * @param verticalMovement the value to set this player's vertical velocity by as an int
+     */
     public void move(final int horizontalMovement, final int verticalMovement) {
         setVelX(horizontalMovement);
         setVelY(verticalMovement);
     }
 
+    /**
+     * If this player's attack is off cooldown the attack timer is reset and true is returned else
+     * false.
+     * @return if this player was able to attack as a boolean
+     */
     public boolean attackIfAllowed() {
         if (getTimeSinceLastShot() >= getAttackSpeed() * getAttackSpeedModifier()) {
             setTimeSinceLastShot(0);
@@ -109,62 +131,110 @@ public class Player extends GameEntity {
         }
     }
 
-    public int getTimeSinceLastShot() {
+    private int getTimeSinceLastShot() {
         return timeSinceLastShot;
     }
 
-    public void setTimeSinceLastShot(int timeSinceLastShot) {
+    private void setTimeSinceLastShot(int timeSinceLastShot) {
         this.timeSinceLastShot = timeSinceLastShot;
     }
 
+    /**
+     * Gets this player's projectileSize.
+     * @return the projectileSize of this player as a int
+     */
     public int getProjectileSize() {
         return projectileSize;
     }
 
+    /**
+     * Gets this player's projectileShape.
+     * @return the projectileShape of this player as a ProjectileShape
+     */
     public ProjectileShape getProjectileShape() {
         return projectileShape;
     }
 
+    /**
+     * Gets this player's projectileColour.
+     * @return the projectileColour of this player as a ProjectileColour
+     */
     public ProjectileColour getProjectileColour() {
         return projectileColour;
     }
 
+    /**
+     * Gets this player's projectileTeam.
+     * @return the projectileTeam of this player as a ProjectileTeam
+     */
     public ProjectileTeam getProjectileTeam() {
         return projectileTeam;
     }
 
-    public int getCurrentHealthPoints() {
+    private int getCurrentHealthPoints() {
         return currentHealthPoints;
     }
 
-    public int getAttackSpeed() {
+    private int getAttackSpeed() {
         return attackSpeed;
     }
 
-    public float getAttackSpeedModifier() {
+    private float getAttackSpeedModifier() {
         return attackSpeedModifier;
     }
 
+    /**
+     * Gets this player's base attack damage.
+     * @return the attack damage of this player as a float
+     */
     public float getDamage() {
         return damage;
     }
 
+    /**
+     * Gets this player's damage modifier.
+     * @return the damage modifier of this player as a float
+     */
     public float getDamageModifier() {
         return damageModifier;
     }
 
+    /**
+     * Gets this player's bulletSpeed.
+     * @return the bulletSpeed of this player as a float
+     */
     public float getBulletSpeed() {
         return bulletSpeed;
     }
 
+    /**
+     * Gets this player's bulletSpeedModifier.
+     * @return the bulletSpeedModifier of this player as a float
+     */
     public float getBulletSpeedModifier() {
         return bulletSpeedModifier;
     }
 
+    /**
+     * Gets this player's bulletLifespan.
+     * @return the bulletLifespan of this player as a int
+     */
     public int getBulletLifespan() {
         return bulletLifespan;
     }
 
+    /**
+     * Gets this player's body position.
+     * @return the body position of this player as a Vector2
+     */
+    public Vector2 getPosition() {
+        return getBody().getPosition();
+    }
+
+    /**
+     * Deals damage to this player's current health.
+     * @param damage the damage to deal to this player's health as a float.
+     */
     public void takeDamage(final float damage) {
         if (damage >= 0) {
             setCurrentHealthPoints(-damage); // ensure damage will reduce health
@@ -173,11 +243,11 @@ public class Player extends GameEntity {
         }
     }
 
+    /**
+     * Changes this player's health by a value.
+     * @param valueToChangeHealthBy the value to be added to this player's health
+     */
     private void setCurrentHealthPoints(final float valueToChangeHealthBy) {
         currentHealthPoints += Math.round(valueToChangeHealthBy);
-    }
-
-    public Vector2 getPosition() {
-        return getBody().getPosition();
     }
 }
